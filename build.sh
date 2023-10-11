c10f22df7234a7a6eb3629f1bcec075f98c96e0d
Petr Muzikant <petr.muzikant@cyber.ee>
2023-10-11 14:02:20 +0200

    fix building

--- build.sh
+++ build.sh
@@ -2,15 +2,21 @@
 rm -rf build
 mkdir -p build
 
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-if [ -v LIBOQS_ROOT ] && [ -e $LIBOQS_ROOT ]; then
+if [[ -e "$LIBOQS_ROOT" ]] || [[ -e "$script_dir/liboqs" ]]; then
     echo "liboqs directory already exists, skipping cloning"; \
 else \
     git clone -b main https://github.com/open-quantum-safe/liboqs.git; \
     export LIBOQS_ROOT=$(pwd)/liboqs; \
 fi
 
-cmake -GNinja -B $LIBOQS_ROOT/build liboqs && ninja -j $(nproc) -C $LIBOQS_ROOT/build
+if [[ -e "$LIBOQS_ROOT/build" ]]; then
+    echo "liboqs library already builded, skipping compilation"; \
+else \
+    rm -rf $LIBOQS_ROOT/build; \
+    cmake -GNinja -B $LIBOQS_ROOT/build $LIBOQS_ROOT && ninja -j $(nproc) -C $LIBOQS_ROOT/build; \
+fi
 
 # Compile the C++ wrapper
 swig -php -c++ -o ./build/oqsphp_wrap.cpp -I$LIBOQS_ROOT/build/include oqsphp.i
@@ -21,3 +27,4 @@ gcc -std=c++20 -O2 -fPIC `php-config --includes` -I$LIBOQS_ROOT/build/include -c
 
 # Create the PHP wrapper
 gcc -std=c++20 -shared ./build/oqsphp_wrap.o -L$LIBOQS_ROOT/build/lib -loqs -o ./build/oqsphp.so
+echo "Finished"
